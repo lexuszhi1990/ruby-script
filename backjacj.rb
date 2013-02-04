b29a6335aaba557a8bbb552f66bd0639c8d8c01f
lexus <lexuszhi1990@gmail.com>
2013-02-04 17:21:52 +0800

    add the loop

--- backjacj.rb
+++ backjacj.rb
@@ -26,37 +26,104 @@ def initpoker(frequence)
 end
 
 #p = initpoker(2)
+#puts cards = (1...11).to_a
 
-puts cards = ["A"] + (2...10).to_a.collect!{ |x| x.to_s } + ["10"]
+cards = (1...11).to_a.collect! { |x| x.to_s }
+puts cards.to_s
 
 points = { player: 0, dealer: 0}
 player = Array.new(0)
 dealer = Array.new(0)
 list = Array.new(0)
 
-card = cards.pop(2)
-puts card.to_s
+if(cards.length < 9)
+  puts "the card is wrong"
+  return
+end
+card = cards.pop()
+list.push(card)
+player.push(card)
+card = cards.pop()
 list.push(card)
 player.push(card)
 
-card = cards.pop(2)
-puts card.to_s
+card = cards.pop()
 list.push(card)
 dealer.push(card)
+card = cards.pop()
+list.push(card)
+    dealer.push(card)
 
-puts "player, please make a choice : hit or stay ... :)"
-choice = gets.chomp
-if(choice == "hit")
-
-else if(choice == "stay")
-
-    else
-    
-    end
+def calculate(person)
+  sum = 0
+  person.each do |x|
+    sum = sum + x.to_i
+  end
+  sum
 end
 
+loop do
+    pstat = false
+    dsate = false
+
+    puts "player, please make a choice : hit or stay ... :)"
+    choice = gets.chomp
+    if(choice == "hit")
+        pstat = false
+
+        card = cards.pop()
+        puts card.to_s
+        list.push(card)
+        player.push(card)
+        points[:player] = calculate(player)
+
+        if points[:player] > 21
+            puts "Player lose"
+        end
+        if points[:player] == 21
+            puts "Player win"
+        end
+        puts "player's points is " +  points[:player].to_s
+
+    else
+        pstat = true
+        puts "you choose to stay... now switch to dealer"  
+    end
+
+    puts "dealer, please make a choice : hit or stay ... :)"
+    choice = gets.chomp
+    if(choice == "hit")
+        dstat = false
+
+        card = cards.pop()
+        puts card.to_s
+        list.push(card)
+        dealer.push(card)
+        points[:dealer] = calculate(dealer)
+
+        if points[:dealer] > 21
+            puts "dealer lose"
+        end
+        if points[:dealer] == 21
+            puts "dealer win"
+        end
+        puts "dealer's points is " +  points[:dealer].to_s
 
 
+    else
+        dstat = true
+        puts "you choose to stay... now switch to player"  
+    end
+
+
+
+
+
+    if(pstat == true && dstat == true) 
+      
+      break;
+    end
+end
 
 
 
