785176d33961afd61e2fa9c016e83bc714a84474
lexus <lexuszhi1990@gmail.com>
2013-02-04 16:06:46 +0800

    use collect to convert int to str for each of the array

--- backjacj.rb
+++ backjacj.rb
@@ -1,26 +1,35 @@
 #!/usr/bin/env ruby
 
 def initpoker(frequence)
-  poker  = Array.new
+  poker  = Array.new()
   i = 0;
   while(poker.length < frequence * 13)
     loop do  i = rand(14); break if i != 0  end
-    
-    if(poker.count(i) < frequence)
-      poker.push(i)
+
+    if(poker.count(i.to_s) < frequence)
+        poker.push(i.to_s)
     end
   end
 
+  
+  puts poker.to_s
   puts poker.sort.to_s
+
+  poker.collect! do |x|
+    if(x == "1"); x = "A"; end
+    if(x == "11"); x = "J"; end
+    if(x == "12"); x = "Q"; end
+    if(x == "13"); x = "K"; end
+    x
+  end
   puts poker.to_s
 
   return poker
 end
 
-#p = initpoker(4)
+p = initpoker(2)
 
-
-
+cards = ["A"] + (2...10).to_a.collect!{ |x| x.to_s }
 
 
 
